cda7ba6bb8a4cf75683e0b697e1a254fc8a17800
Natalie Klein <agent229@gmail.com>
2008-06-18 17:00:47 -0600

    focusing on WeightChromosome, fleshing out methods

--- lib/oscillator_genetic_algorithm.rb
+++ lib/oscillator_genetic_algorithm.rb
@@ -28,40 +28,47 @@ module OscillatorGeneticAlgorithm
    
     attr_accessor :population
 
-    # TODO see where chromosome initialization comes from, how to set it to weight/param type...  
-    def initialize(initial_population_size, generations)
-      srand(1) # For repeatability, can change/record seed
+    # Creates new GeneticSearch
+    #   initial_population_size: the size of the population to evolve
+    #   generations: the number of generations to evolve
+    #   seed: a random number seed for this entire run, for repeatability reasons
+    #   chrom_size: describes the size of any given chromosome
+    def initialize(initial_population_size, generations, chrom_size, seed)
+      # Seed random number gen, initialize fields
+      srand(seed)                       
       @population_size = initial_population_size
       @max_generation = generations
       @generation = 0
+      @chrom_size = chrom_size
     end
     
-    # Runs the genetic algorithm as described above, then returns the best chromosome
+    # Runs the genetic algorithm as described above, returning the best chromosome on completion
     def run
-      generate_initial_population                    #generate initial population 
+      generate_initial_population                     # generate initial population 
       @max_generation.times do
-        selected_to_breed = selection                #evaluates current population 
-        offsprings = reproduction selected_to_breed  #generate the population for this new generation
-        replace_worst_ranked offsprings
+        selected_to_breed = selection                 # evaluate current population 
+        offsprings = reproduction(selected_to_breed)  # generate the population for this new generation
+        replace_worst_ranked(offsprings)              # replace the worst members
       end
       return best_chromosome
     end
-    
+   
+    # Generates the initial population of chromosomes according to that chromosome's seed method
     def generate_initial_population
      @population = []
      @population_size.times do
-       population << chromosome.seed # TODO which chromosome is it using? how do i set it?
+       chromosome = WeightChromosome.new(@chrom_size) # For now, just a WeightChromosome
+       population << chromosome.seed                  # Uniformly distributed values
      end
     end
     
-    # selection is the stage of a genetic algorithm in which individual 
+    # Selection is the stage of a genetic algorithm in which individual 
     # genomes are chosen from a population for later breeding. 
-    # there are several generic selection algorithms, such as 
-    # tournament selection and roulette wheel selection. we implemented the
+    # There are several generic selection algorithms, such as 
+    # tournament selection and roulette wheel selection. We implemented the
     # latest.
     # 
-    # steps:
-    # 
+    # Steps:
     # 1. the fitness function is evaluated for each individual, providing fitness values
     # 2. the population is sorted by descending fitness values.
     # 3. the fitness values ar then normalized. (highest fitness gets 1, lowest fitness gets 0). 
@@ -136,153 +143,66 @@ module OscillatorGeneticAlgorithm
     end
   end
 
-  # A ParamChromosome describes one of the parameters of an oscillator (with one entry
-  # for each oscillator in the network). This could be natural phase, amplitude, or frequency.
-  # Thus the number of data in the chromosome should be the number of oscillators in the network.
-  # The chromosome data will be normalized on [0,1) so that it may be used for different purposes;
-  # the user can simply multiply by the appropriate scale once they recieve the data.
-  class ParamChromosome
-    
-    attr_accessor :data
-    attr_accessor :normalized_fitness
-    
-    def initialize(data)
-      srand(1) # For repeatability, can change/record seed
-      @data = data
-    end
-    
-    # The fitness function quantifies the optimality of a solution 
-    # (that is, a chromosome) in a genetic algorithm so that that particular 
-    # chromosome may be ranked against all the other chromosomes. 
-    # In this case, the fitness function is essentially feeding the parameters into
-    # the network simulation and measuring the error compared to the desired output. 
-    def fitness
-      return @fitness if @fitness
-      #TODO
-      return @fitness
-    end
-
-    # For ParamChromosomes, mutation means random, small noise added with some chance to
-    # any given parameter in the chromosome to maintain genetic diversity. Because the
-    # parameters are normalized in the GA, the noise added definitely needs to be in (-1,1)
-    # but should be quite small. This will perturb the system to avoid minima. 
-    def self.mutate(chromosome)
-      data = chromosome.data
-      0.upto(data.length-1)
-        data = chromosome.data
-        if chromosome.normalized_fitness && rand < ((1 - chromosome.normalized_fitness) * 0.3)
-          data[index] += rand(0.25)-0.125
-          chromosome.data = data
-          @fitness = nil
-        end
-      end
-    end
-    
-    # Reproduction is used to vary the programming of a chromosome or 
-    # chromosomes from one generation to the next. There are several ways to
-    # combine two chromosomes: One-point crossover, Two-point crossover,
-    # "Cut and splice", edge recombination, and more. 
-    # 
-    # The method is usually dependant of the problem domain.
-    # In this case, we have implemented edge recombination, wich is the 
-    # most used reproduction algorithm for the Travelling salesman problem.
-    # TODO see if this is suitable for this purpose...
-    def self.reproduce(a, b)
-      data_size = @@costs[0].length
-      available = []
-      0.upto(data_size-1) { |n| available << n }
-      token = a.data[0]
-      spawn = [token]
-      available.delete(token)
-      while available.length > 0 do 
-        #Select next
-        if token != b.data.last && available.include?(b.data[b.data.index(token)+1])
-          next_token = b.data[b.data.index(token)+1]
-        elsif token != a.data.last && available.include?(a.data[a.data.index(token)+1])
-          next_token = a.data[a.data.index(token)+1] 
-        else
-          next_token = available[rand(available.length)]
-        end
-        #Add to spawn
-        token = next_token
-        available.delete(token)
-        spawn << next_token
-        a, b = b, a if rand < 0.4
-      end
-      return ParamChromosome.new(spawn)
-    end
-    
-    # Initializes an individual solution (chromosome) for the initial 
-    # population. Generates random numbers in [0,1) for each parameter. 
-    def self.seed
-      data_size = @@costs[0].length # need other way to get data size...
-      0.upto(data_size-1) 
-        seed << rand(1)
-      end 
-      return ParamChromosome.new(seed)
-    end
-
-    def self.set_cost_matrix(costs)
-      @@costs = costs
-    end
-  end
-
   # A WeightChromosome relates to the weights between the nodes in the network. This can be
   # best expressed as a matrix of connections, where 0 represents no connection and anything
   # else represents the weight, where the ijth element designates a connection between node i and j.
   # There should be no crossover here as it doesn't make sense for this type of net; mutation only.
+  # In this case, all connections are free game for mutation.... should probably be changed eventually
   class WeightChromosome
     
     attr_accessor :data
     attr_accessor :normalized_fitness
-    
-    def initialize(data)
-      srand(1) # For repeatability, can change/record seed
-      @data = data
+    attr_accessor :size
+   
+    # Data will be a size x size  matrix, where size is the number of nodes
+    def initialize(size)
+      @size = size 
+      @data = GSL::Matrix.alloc(@size) # Allocate an empty matrix for this set of weights
     end
     
-    # Should be essentialy same as fitness function for ParamChromosome
     def fitness
       return @fitness if @fitness
-      #TODO
+      #TODO write propagation so this works... fitness weights.... etc
       return @fitness
     end
 
-    # Random noise added to weights, but this time only to nonzero weights. Zero weights represent
-    # lack of any connection and should be left alone completely. We are only evolving the non-
-    # zero weights.    
-    def self.mutate(chromosome)
-      if chromosome.normalized_fitness && rand < ((1 - chromosome.normalized_fitness) * 0.3)
-        #TODO
-        chromosome.data = data
-        @fitness = nil
+    # Random noise added to weights
+    #   chromosome: the chromosome to mutate
+    #   mutation_rate: a tunable parameter describing chance of mutation
+    #   mutation_size: a tunable parameter describing the maximum size of the mutation
+    #   pct_weights_mutated: a tunable parameter describing the percentage of the weights that gets mutated if
+    #                        the chromosome is selected for mutation
+    def self.mutate(chromosome, mutation_rate, mutation_size, pct_weights_mutated)
+      if chromosome.normalized_fitness && rand < ((1 - chromosome.normalized_fitness) * mutation_rate)
+        1.upto(pct_weights_mutated*chromosome.size*chromosome.size) do
+          data[rand(chromosome.size),rand(chromosome.size)] += (rand(2*mutation_size) - mutation_size) 
+        end
+      @fitness = nil
       end
     end
     
     # Reproduction here involves no crossover, simply duplication. 
     def self.reproduce(a, b)
-      data_size = @@costs[0].length
-      available = []
-      0.upto(data_size-1) { |n| available << n }
-      token = a.data[0]
-      spawn = [token]
-      available.delete(token)
-      while available.length > 0 do 
-        #Add to spawn
-        token = next_token
-        available.delete(token)
-        spawn << next_token
+      offspring = WeightChromosome.new(a.size)
+      0.upto(a.size) do |row|
+        0.upto(a.size) do |col|
+          offspring.data[row,col]=a.data[row,col]
+        end
       end
-      return WeightChromosome.new(spawn)
+      return offspring
     end
     
-    # Initializes an individual solution (chromosome) for the initial 
-    # population. Usually the chromosome is generated randomly, but you can 
-    # use some problem domain knowledge, to generate better initial solutions.
+    # Initializes an individual solution for the initial population.
+    # Right now, simply fills the data matrix with evenly distributed values on [0,1)
     def self.seed
-      # TODO
+      0.upto(@size-1) do |row|
+        0.upto(@size-1) do |col|
+          @data[row,col] = rand
+        end
+      end
     end
 
+    # TODO want to use this? how would i set cost matrix?
     def self.set_cost_matrix(costs)
       @@costs = costs
     end
