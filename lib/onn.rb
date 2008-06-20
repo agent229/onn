8aa359c8dd599fcb07c78626c747064e046dd530
Natalie Klein <agent229@gmail.com>
2008-06-20 11:30:32 -0600

    converted data structures and updated methods appropriately

--- lib/onn.rb
+++ lib/onn.rb
@@ -1,56 +1,78 @@
-# GENERAL TODO: modify everything to use the new node objects, hashes, etc. create methods to automate the conversions
-# from matrices to these objects and hashes
-
 module OscillatorNeuralNetwork
   
   # This is an implementation of oscillator neural networks which uses a genetic
   # algorithm as the learning rule. It can learn by adjusting either connection weights
-  # or the "natural" properties of the oscillators, depending on the
-  # type of genetic algorithm class selected from the OscillatorGeneticAlgorithm module.
+  # or the "natural" properties of the oscillators, depending on the need of the user.
   #
-  # The general network data structures are Matrices as defined in Ruby/GSL. The rows always
+  # The general network data structures are input as 2D (nested) arrays. The rows always
   # correspond to a particular node, and columns to attributes or data associated with that node.
   #
   # This is based on the neural network from the ai4r library.
  
-  require("rbgsl")                       # For Matrix structures
   require 'oscillator_genetic_algorithm' # For genetic algorithm classes
-  import OscillatorGeneticAlgorithm      # For genetic algorithm classes
+  import OscillatorGeneticAlgorithm      
 
+  # Custom Hash method to convert an array of keys and an array of values to a Hash
+  #  keys: an array containing the ordered keys
+  #  values: an array containing the ordered values
+  # Usage:
+  #   Hash.create(['Oriole','Spaniel'],['bird','dog']) => {"Oriole"=>"bird", "Spaniel"=>"dog"}
+  class << Hash
+    def create(keys, values)
+      self[*keys.zip(values).flatten]
+    end
+  end
+
+  # Network class definition
   class GeneticAlgorithmONN 
 
-    # Initializes an ONN of n nodes with any structure and coupled universal oscillators. 
-    #   connections: an nxn, weighted connections Matrix (entry range: 0-1) where the ijth weight corresponds
-    #                to a weighted, asymmetric connection from node i to node j
-    #   states: a Matrix of the natural properties of the nodes, ordered in the same way as the
-    #           connections matrix, with the same row indices, data in columns
-    def initialize(connections, states)
-    
-      # Create the network, which is going to be an indexed list of OscillatorNeuron objects
-      @nodes = []
-      0.upto(states.size1) do |index|
-        # TODO create nstate hash from the states matrix somehow
-        @nodes << OscillatorNeuron.new(index, nstate) 
-      end
+    attr_accessor :nodes
 
-      # Now that the network is created, go through and update the connections TODO
-      0.upto(connections.size1) do |row|
-        0.upto(connections.size1) do |col|
-          if connections[row,col] != 0
-            # TODO convert into connection, store in appropriate node
-          end
-        end
-     end
+    # Initializes an ONN of n nodes with any structure and coupled universal oscillators. 
+    #   connections: an nxn, weighted 2D connections array (entry range: 0-1) where the ijth weight corresponds
+    #                to a weighted, asymmetric connection from node i to node j
+    #   state_names: an array of the names of the state variables provided in state_vals, in the same
+    #                order as the columns in state_vals
+    #   state_vals: a 2D array of the natural properties of the nodes, ordered in the same way as the
+    #           connections array, with the same row indices, data in columns
+    # Usage:
+    #  net = GeneticAlgorithmONN.new([[0,0],[1,1]],['phase','freq'],[[0.2,0.3],[0.5,0.6]])
+    def initialize(connections, state_names, state_vals)
+    
+      # Create the network, which is an indexed list of OscillatorNeuron objects
+      @nodes = []
+      @state_names = state_names
+      state_vals.each { |nstate| @nodes << OscillatorNeuron.new(Hash.create(@state_names, nstate)) } 
+      # Put nodes back in correct index order
+      @nodes.reverse!
+
+      # Now that the network is created, go through and update the connections 
+      update_connections(connections)
 
     end
 
-    # Evaluates new input, given in matrix form. 
-    #   input_state: a new input in matrix form.
+    # Updates the connections between nodes based on the connections matrix
+    #  connections: the 2D (nested) array of weighted connections
+    def update_connections(connections)
+      connections.each_index { |row_index|
+        row.each_index { |col_index|
+          this_conn = connections[row_index][col_index]
+          if this_conn != 0.0
+            @nodes[row_index].out_conns[@nodes[col_index]] = this_conn 
+            @nodes[col].in_conns[@nodes[row]] = this_conn 
+          end
+        }
+      } 
+
+    end
+
+    # Evaluates new input, given in 2D array form. 
+    #   input_state: a new input.
     def eval(input_state)
 
       # Set new input states
-      0.upto(input_state.size1) do |index|
-        @nodes[index].state = #TODO make a "create hash from state data" method to call here and above 
+      0.upto(input_state.length) do |index|
+        @nodes[index].state = Hash.create(@state_names,input_state[index])  
       end
 
       # Traverse network and drive oscillations and calculate output TODO 
@@ -61,19 +83,17 @@ module OscillatorNeuralNetwork
     end
 
     # This method trains the network using an instance of an OscillatorGeneticAlgorithm.
-    #   input: Networks input (matrix form)
-    #   output: Expected output for the given input (matrix form).
+    #   input: Networks input (nested/2D array form)
+    #   output: Expected output for the given input (nested/2D array form).
+    #   pop_size: how many solutions to keep in the population
+    #   gens: how many generations to run the GA
+    #   seed: a PRNG seed governing all PRNG uses in this run (for repeatability)
     #
     # Returns: the difference between real output and the expected output
-    def train(input, output)
+    def train(input, output, pop_size, gens, seed)
       # This entire thing just needs to call the genetic algorithm for training..... TODO
-      # GA then calls the eval method to obtain its results, weight the fitness, etc. 
-      # Return net error by propagating thru with result of GA
-      # Return of GA will be the network.
-      # Different train methods?? (trainWeights, trainAmps....) or is it possible to just modify that some other way
-      pop_size = 10;
-      gens = 10;
-      seed = 1
+      # Determine structure of params TODO
+      params = Hash.new 
       ga = GeneticSearch.new(@nodes, pop_size, gens, seed, params)
       @nodes = ga.run
       eval(@input_states)
@@ -82,33 +102,19 @@ module OscillatorNeuralNetwork
   end
 
   # This class keeps track of the state of a single OscillatorNeuron. Each neuron knows everything about
-  # its own current state and connections (both inbound and outbound connections).
+  # its own natural state, current state, and inbound/outbound connections.
   class OscillatorNeuron
 
-    attr_accessor :in_conns # A list of exiting OscillatorNeuron objects which point here hashed with conn weights
-    attr_accessor :out_conns # A list of exiting OscillatorNeuron objects which this points at hashed with conn weights
-    attr_accessor :state # State will likely be a hash, containing all variables and a type (input, hidden, output)?
+    attr_accessor :in_conns # A list of existing OscillatorNeuron objects which point here hashed with conn weights
+    attr_accessor :out_conns # A list of existing OscillatorNeuron objects which this points at hashed with conn weights
+    attr_accessor :state # State is a hash, containing all information (besides connections)
 
     # Initialize a new OscillatorNeuron by passing a state hash.
-    #  state: a has describing all of the state variables (TBD contents.... TODO)
-    #  index: the node's original index from the connections/state matrices
-    def initialize(index,state)
-      @index = index
+    #  state: a hash describing all of the state variables 
+    def initialize(state)
       @state = state
       @in_conns = Hash.new 
       @out_conns = Hash.new 
-    end
-
-    # Instantiates the list of inbound connections
-    def set_in_conns(in_conns)
-      #TODO FIX
-      @in_conns = in_conns
-    end
-
-    # Adds an outbound connection to the list
-    def add_out_conn(out_conn)
-      # TODO FIXME to use hash
-      @out_conns << out_conn
     end
 
   end
