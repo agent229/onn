39ebe8320f48cde2b71e1e4e5b3dc82608deb77c
Natalie Klein <agent229@gmail.com>
2008-06-23 14:16:12 -0600

    added "next state" and "time step"

--- lib/onn.rb
+++ lib/onn.rb
@@ -35,13 +35,15 @@ module OscillatorNeuralNetwork
     #                order as the columns in state_vals
     #   state_vals: a 2D array of the natural properties of the nodes, ordered in the same way as the
     #           connections array, with the same row indices, data in columns
+    #   t_step: discrete time step to be used in simulation to update oscillators
     # Usage:
-    #  net = GeneticAlgorithmONN.new([[0,0],[1,1]],['phase','freq'],[[0.2,0.3],[0.5,0.6]])
-    def initialize(connections, state_names, state_vals)
+    #  net = GeneticAlgorithmONN.new([[0,0],[1,1]],['phase','freq'],[[0.2,0.3],[0.5,0.6]],0.01)
+    def initialize(connections, state_names, state_vals, t_step)
     
       # Create the network, which is an indexed list of OscillatorNeuron objects
       @nodes = []
       @state_names = state_names
+      @t_step = t_step
       state_vals.each { |nstate| @nodes << OscillatorNeuron.new(Hash.create(@state_names, nstate)) } 
       # Put nodes back in correct index order
       @nodes.reverse!
@@ -54,15 +56,18 @@ module OscillatorNeuralNetwork
     # Updates the connections between nodes based on the connections matrix
     #  connections: the 2D (nested) array of weighted connections
     def update_connections(connections)
-      connections.each_index { |row_index|
-        connections[row_index].each_index { |col_index|
-          this_conn = connections[row_index][col_index]
-          if this_conn != 0.0
-            @nodes[row_index].out_conns[@nodes[col_index]] = this_conn 
-            @nodes[col_index].in_conns[@nodes[row_index]] = this_conn 
-          end
-        }
-      } 
+      
+      connections.each_index 
+        { |row_index|
+          connections[row_index].each_index 
+            { |col_index|
+              this_conn = connections[row_index][col_index]
+                if this_conn != 0.0
+                  @nodes[row_index].out_conns[@nodes[col_index]] = this_conn 
+                  @nodes[col_index].in_conns[@nodes[row_index]] = this_conn 
+                end
+            }
+         } 
 
     end
 
@@ -71,13 +76,19 @@ module OscillatorNeuralNetwork
     def eval(input_state)
 
       # Set new input states
-      input_state.each_index { |index| @nodes[index].state = Hash.create(@state_names,input_state[index]) }
+      change_input(input_state)
 
       # Traverse network and drive oscillations and calculate output TODO 
+      #1. 
 
       # Return the calculated output
       return @output
 
+    end
+
+    # Resets the input states
+    def change_input(new_input)
+      new_input.each_index { |index| @nodes[index].state = Hash.create(@state_names,new_input[index]) }
     end
 
     # This method trains the network using an instance of an OscillatorGeneticAlgorithm.
@@ -86,13 +97,15 @@ module OscillatorNeuralNetwork
     #   pop_size: how many solutions to keep in the population
     #   gens: how many generations to run the GA
     #   seed: a PRNG seed governing all PRNG uses in this run (for repeatability)
+    #   params: a Hash describing which quantities should be modified in evolution,
+    #           and in what proportion. Eg {'weights'=>0.25,'freqs'=>0.75}
     #
     # Returns: the difference between real output and the expected output
     def train(input, output, pop_size, gens, seed, params)
       # This entire thing just needs to call the genetic algorithm for training..... TODO
-      # Determine structure of params TODO
-      ga = GeneticSearch.new(@nodes, pop_size, gens, seed, params)
-      @nodes = ga.run
+      change_input(input)
+      ga = GeneticSearch.new(self, pop_size, gens, seed, params)
+      self = ga.run
       eval(@input_states)
     end
     
@@ -104,14 +117,24 @@ module OscillatorNeuralNetwork
 
     attr_accessor :in_conns # A list of existing OscillatorNeuron objects which point here hashed with conn weights
     attr_accessor :out_conns # A list of existing OscillatorNeuron objects which this points at hashed with conn weights
-    attr_accessor :state # State is a hash, containing all information (besides connections)
+    attr_accessor :curr_state # State is a hash, containing all information (besides connections)
+    attr_accessor :next_state # State is a hash, containing all information (besides connections)
 
     # Initialize a new OscillatorNeuron by passing a state hash.
     #  state: a hash describing all of the state variables 
     def initialize(state)
-      @state = state
+      @curr_state = state
       @in_conns = Hash.new 
       @out_conns = Hash.new 
+      @next_state = nil
+    end
+
+    # If there is a complete next state stored, updates the current state.
+    def update_state
+      if @next_state
+        @curr_state = @next_state
+        @next_state = nil
+      end
     end
 
   end
