8cd4c6c82493b60729740c37af1ce5e6364a1d74
Natalie Klein <agent229@gmail.com>
2008-07-02 14:46:47 -0600

    refactered, fixed error estimation

--- lib/onn.rb
+++ lib/onn.rb
@@ -21,38 +21,47 @@ module OscillatorNeuralNetwork
     end
   end
 
-  # "Genetic algorithm oscillator neural network" (GAONN) class definition
+  # "Genetic algorithm oscillator neural network" (GAONN) class definition describes an
+  # entire network object through a list of nodes
   class GAOscillatorNeuralNetwork 
    
-    # Access to the array of OscillatorNeuron objects comprising the network
     attr_accessor :nodes
-    # Ready-only access to other fields (for testing/data output)
     attr_reader :t_step
     attr_reader :num_nodes
     attr_reader :seed
 
-    # Initializes an ONN of coupled sinusoidal oscillators. (simple version)
+    # Initializes an ONN of coupled sinusoidal oscillators. 
     #   t_step: discrete time step to be used in simulation to update oscillators
-    #   num_nodes: the number of nodes to create in the network
+    #           (smaller -> closer to continuous time)
+    #   num_nodes: the number of nodes that will exist in the network
     #   seed: a PRNG seed governing all PRNG uses in this run (for repeatability)
     def initialize(t_step, num_nodes, seed)
-      # Initialize some instance variables
+      # Initialize instance variables from parameters
       @t_step = t_step
-      @seed = seed
-      @nodes = []
       @num_nodes = num_nodes
-      # TODO un-hard-code names?
+      @seed = seed
+      
+      # Create empty list of nodes
+      @nodes = []
+
+      # The array of names which describe modifiable features of the oscillators
       @state_names = [:freq, :phase]
+
       # Seed PRNG for this run
       srand(seed)
     end
 
     # Stores input data array as list of OscillatorNeuron objects instantiated with data.
+    #   state_vals: a nested array of data describing OscillatorNeurons. Each entry is a list of
+    #               properties in the order described by @state_names
     def set_nodes(state_vals)
-      state_vals.each { |nstate| @nodes << OscillatorNeuron.new(Hash.create(@state_names, nstate)) } 
+      state_vals.each do |nstate| 
+        @nodes << OscillatorNeuron.new(Hash.create(@state_names, nstate))
+      end
     end
 
-    # Generates 'random' set of node data (random natural states)
+    # Generates a nested data array with random set of node data (random natural states).
+    # Uses @num_nodes to determine how many nodes worth of data to generate.
     def generate_random_node_data
       data = []
       @num_nodes.times do
@@ -65,9 +74,11 @@ module OscillatorNeuralNetwork
       return data
     end
 
-    # Updates the connections between nodes based on the connections 2D array/matrix.
+    # Updates the connections between nodes based on a connections 2D array.
     # Stores the connection information in the approproiate node's in_conns and out_conns fields.
-    #  connections: the 2D (nested) array of weighted connections
+    #  connections: the 2D (nested) array of weighted connections, where the row, R, refers to the
+    #               node the connection is coming from, and the column C, when nonzero, refers
+    #               to the weighted connection to the Cth node
     def set_connections(connections)
      # Iterate through each entry in the 2D array
       connections.each_index do |row_index|
@@ -81,36 +92,38 @@ module OscillatorNeuralNetwork
       end 
     end
 
-    # Evaluates the network's state by propagating through the current input states. To
-    # propagate a enw input state, first call change_input
+    # Evaluates the network's state by propagating the current input states through the network.
+    # Returns a list containing the output states.
     def eval
-      # TODO watch for infinite loops (if nodes are connected to each other...)
-      # Tell every node to propagate its current state to its out_conns
+      # Tell every node to propagate/communicate its current state to its outgoing connections
       @nodes.each do |node|
         node.propagate
       end
-      # After propagation, calculate and update each node's state 
+
+      # After propagation, calculate/update each node's state 
       @nodes.each do |node|
         node.update_state
       end
+
       # Return the calculated output as a 2D array of output nodes
       return get_outputs
     end
 
-    # Retrieves all output nodes by checking which have no out_conns 
+    # Retrieves all output nodes by checking which have no outgoing connections (I define
+    # this to be an output node). Returns a list of output nodes.
     def get_outputs
-      count = 0
       outputs = []
       @nodes.each do |node|
         if node.out_conns.empty?
           outputs << node
         end 
       end
-      return outputs # TODO is order preserved? is order needed?
+      return outputs 
     end
 
-    # Resets the input states manually
-    #  new_input: a 2D array describing the desired state of the input nodes
+    # Replaces the input nodes with new nodes described in the given data array. 
+    #  new_input: a 2D array describing the desired state of the input nodes, which should 
+    #             be given in the same order as the original nodes
     def change_input(new_input)
       new_input.each_index do |index| 
         @nodes[index].curr_state = Hash.create(@state_names,new_input[index]) 
@@ -118,44 +131,63 @@ module OscillatorNeuralNetwork
     end
 
     # This method trains the network using an instance of an OscillatorGeneticAlgorithm.
-    #   input: Network input (nested/2D array form)
-    #   output: Expected/desired output for the given input (nested/2D array form)
-    #   pop_size: how many solutions to evolve in the population
+    #   input: Network input (ordered, nested/2D array form)
+    #   exp_output: Expected/desired output for the given input (ordered, nested/2D array form)
+    #   pop_size: how many solutions to keep in the "potential solution" population
     #   gens: how many generations to run the GA
-    #
-    # Returns: the difference between real output and the expected output
+    #   mutation_rate: a parameter describing how frequently a given solution gets random mutations
+    # Returns a weighted error estimate of how "close" the expected and actual outputs are after
+    # training is complete.
     def train(input, exp_output, pop_size, gens, mutation_rate)
-      # new instance vars for current training run
       @curr_expected = exp_output
-      # Set new input
-      change_input(input)
-      # Instantiate genetic algorithm
+      change_input(input) # Set new input states
+
+      # Instantiate genetic algorithm instance
       ga = GeneticSearch.new(self, pop_size, gens, mutation_rate)
-      # Run genetic algorithm, getting the set of nodes back
+      # Run genetic algorithm, getting the best set of nodes back
       @nodes = ga.run
+
       # Evaluate the result with the new, GA-modified nodes in place
       actual_output = eval
+
       # Calculate and return a weighted error
       err = weighted_error(actual_output, exp_output)
-      @curr_expected = nil
+
       return err
     end
 
-    # Error weighting function. calculates a weighted error measure of the output
-    #   result: the actual result of a network propagation
-    #   expected: the expected/desired result
+    # Error weighting function. Calculates a weighted error measure of the output.
+    #   result: the actual result of a network propagation (list of output nodes)
+    #   expected: the expected/desired result (2D array of data)
+    # TODO improve (very simple right now)
     def weighted_error(result, expected)
-      w_err = 1 # FOR TESTING
-      result.each_index do |node_index|
-        result[node_index].curr_state.each_key do |state_name|
-          # TODO cleanup/fix/figure out
-          # w_err += Math::abs(result[node_index].curr_state[state_name]-expected[node_index].curr_state[state_name])
+      w_err = 0
+      result_arr = nodes_to_data_arr(result)
+      result_arr.each_index do |node_index|
+        result_arr[node_index].each_index do |state_index|
+          w_err += Math::abs(result_arr[node_index][state_index]-expected[node_index][state_index])
         end
       end
       return w_err
     end
 
-    # Fitness function for GA. Returns a normalized fitness value (0-1)
+    # Convenience method that converts a list of node objects into a nested data array
+    # representing them, preserving order. Returns the array.
+    def nodes_to_data_arr(nodelist)
+      data_arr = []
+      nodelist.each do |node|
+        node_data = []
+        @state_names.each do |name|
+          node_data << node.curr_state[name]
+        end
+        data_arr << node_data
+      end
+      return data_arr
+    end
+
+    # GA fitness function. Takes a nodelist and returns a weighted error calculation of the result
+    # compared with the expected/desired result by evaluating the network.
+    #   chromosome: a list of OscillatorNeurons 
     def fitness(chromosome)
      @nodes = chromosome
      output = eval  
@@ -163,15 +195,19 @@ module OscillatorNeuralNetwork
      return err
     end
 
-    # Mutation function. Randomly mutates with a given chance specified by GA
+    # Mutation function. Randomly mutates with a given chance specified by GA.
+    #  chromosome: a nodelist (list of Oscillator Neurons)
+    #  mutation_rate: parameter describing the rate of mutation (chance of random mutation)
+    # Returns the mutated chromosome
     def mutate(chromosome, mutation_rate)
       chromosome.each do |node|
         node.natural_state.each_value do |val|
-        if rand < mutation_rate
-          val += (rand - 0.5)  
+          if rand < mutation_rate
+            val += (rand - 0.5)  
+          end
         end
       end
-      end
+      return chromosome
     end
 
   end
