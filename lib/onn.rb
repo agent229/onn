6dd76eb153d2e945fff520efa849a0da8c79478e
Natalie Klein <agent229@gmail.com>
2008-07-14 11:15:36 -0600

    made node update rules

--- lib/onn.rb
+++ lib/onn.rb
@@ -7,24 +7,21 @@
 module OscillatorNeuralNetwork
  
   # For genetic algorithm
+  # TODO fix hardcoded path
   require '~/Documents/SFI/NN/onn/lib/onn_genetic_algorithm'
   include ONNGeneticAlgorithm      
 
-  # For Ruby/GSL scientific library
+  # For Ruby/GSL scientific library (vectors, matrices, graphing)
   require 'gsl'
-
-  # For plotting utility
-  require '~/Documents/SFI/NN/onn/lib/plotter'
-  include Plot
 
   # "Genetic algorithm oscillator neural network" (GAONN) class definition describes an
   # entire network object through a list of nodes and some parameters
-  class GAOscillatorNeuralNetwork 
+  class GAONN 
   
-    # A nested array where the rows are a layer, and each layer contains OscillatorNeurons which belong to it
+    # An array of OscillatorNeuron objects 
     attr_accessor :nodes
 
-    # Update parameters
+    # Update rule parameters
     attr_reader :time_param
     attr_reader :window_param
     attr_reader :seed
@@ -35,69 +32,89 @@ module OscillatorNeuralNetwork
     DEFAULT_SEED = 0
     DEFAULT_MUTATION_RATE = 0.4
 
-    # Initializes an ONN of coupled harmonic oscillators. 
-    #   node_data: a list of vectors containing the following data: <layer_number, natural_frequency, natural_amplitude>
-    #              (keep each vector in the same order as in the connections matrix for consistency)
-    #   connections: a connections matrix (nested array) of connection strengths in the network where ijth entry corresponds
-    #                to the asymmetric connection from node i to node j, where i and j are the indices within the nodes list
-    #   seed: a PRNG seed governing all PRNG uses in this run (for repeatability)
-    #   time_param: parameter to be used in simulation to help decide how often to update states (scales the minimum period)
-    #   window_param: parameter to be used in simulation to help decide stability; related to a sampling "window size" (scales the max period)
-    def initialize(node_data, connections, seed=DEFAULT_SEED, time_param=DEFAULT_TIME_PARAM, window_param=DEFAULT_WINDOW_PARAM)
+    #### Class method(s) ####
 
-      # Initialize instance parameters
-      @time_param = time_param
-      @window_param = window_param
-      @seed = seed
-
-      # Initialize network of nodes by layer
-      @nodes = create_node_list(node_data)
-      @connections = connections
-      @output_states = [] # Will store evolution of output states for analysis
-
-      # Seed PRNG for this run
-      srand(seed)
+    # This method trains a network using an instance of an OscillatorGeneticAlgorithm.
+    #
+    #   network:       the initial network to train 
+    #   input:         Matrix of inputs to the network 
+    #   exp_output:    Expected/desired output for the given input 
+    #   pop_size:      How many solutions to keep in the "potential solution" population
+    #   gens:          How many generations to run the GA
+    #   mutation_rate: a parameter describing how frequently a given solution gets random mutations
+    #
+    # Returns an array containing the evolved network  along with a weighted error estimate of 
+    # how "close" the expected and actual outputs are after training is complete.
+    #
+    def self.train(network, input, exp_output, pop_size, gens, mutation_rate=DEFAULT_MUTATION_RATE)
+      change_input(input)                                            # Set new input states
+      ga = GeneticSearch.new(network, pop_size, gens, mutation_rate) # Create GA
+      best_net = ga.run                                              # Run GA
+      # TODO determine exactly what gets returned from GA, what is neccessary to run it for error
+      actual_output = eval                                           # Evaluate trained network and get error
+      err = weighted_error(actual_output, exp_output)
+      return [best_net, err]
     end
 
-    # Creates the nested list of layers containing node objects with the specified nodes in each layer
-    #   node_data: a list of vectors containing the following data: <layer_number, natural_frequency, natural_amplitude>
+    #### Network instance methods ####
+
+    # Initializes an ONN of coupled harmonic oscillators. 
+    #
+    #   node_data:    a GSL::Matrix containing the following row vectors: <layer_number, natural_frequency, initial_amplitude>
+    #   connections:  a GSL::Matrix of connection strengths in the network. ijth entry is connection from node i to node j
+    #   seed:         a PRNG seed governing all PRNG uses in this run (for repeatability)
+    #   time_param:   parameter to be used in simulation to help decide how often to update states (scales the minimum period)
+    #   window_param: parameter to be used in simulation to help decide stability (scales the max period)
+    #
+    def initialize(node_data, connections, seed=DEFAULT_SEED, time_param=DEFAULT_TIME_PARAM, window_param=DEFAULT_WINDOW_PARAM)
+      @time_param = time_param                          # Set parameters
+      @window_param = window_param
+      @seed = seed
+      @nodes = create_node_list(node_data, connections) # Initialize network of nodes by layer
+      @connections = connections                        # Store connections GSL::Matrix
+      @curr_time = 0.0                                  # Set current time to 0
+      srand(seed)                                       # Seed PRNG for this run
+
+      # TODO calculate time step
+    end
+
+    # Creates the list of OscillatorNeuron objects which contain the data 
+    #
+    #   node_data:   GSL::Matrix with rows containing the following data: <layer_number, natural_frequency, initial_amplitude>
+    #   connections: GSL::Matrix of connection strengths
+    #
     # Returns the list of nodes.
-    def create_node_list(node_data)
+    def create_node_list(node_data, connections)
       nodes = []
-      node_data.each do |node_datum|
-        node_layer = node_datum[0]
-        if !nodes[node_layer]
-          nodes[node_layer] = []
-        end
-        nodes[node_layer] << OscillatorNeuron.new(node_datum) 
+      node_data.each_row do |node_datum|
+        nodes << OscillatorNeuron.new(node_datum,self)    # Initialize node states
       end
+      nodes = set_conns_from_mat(nodes, connections) # Set connections
       return nodes
     end
 
-    # Evaluates the network's state by propagating the current input states through the network.
+    # Evaluates the network's output state by propagating the current input states through the network.
     # Returns a list containing the output states after sufficient stabilizing time.
     # TODO decide how many times to propagate... how many time steps...
     def eval
 
       # Tell every node to propagate/communicate its current state to its outgoing connections
-      @nodes.each do |layer|
-        layer.each do |node|
-          node.propagate
-        end
+      @nodes.each do |node|
+        node.propagate
       end
 
       # After propagation, calculate/update each node's state 
-      @nodes.each do |layer|
-        layer.each do |node|
-          node.update_state
-        end
+      @nodes.each do |node|
+        node.update_state
       end
 
+      increment_time
+
       # Store this set of output states for stability analysis
-      @output_states << @nodes.last
+      @output_states << get_outputs
 
       # Return the calculated output as an array/list of output nodes
-      return @nodes.last 
+      return @output_states.last
     end
 
     # Error weighting function. Calculates a weighted error measure of the output.
@@ -106,7 +123,7 @@ module OscillatorNeuralNetwork
     def weighted_error(result, expected)
       w_err = 0
       result.each_index do |node_index|
-        w_err += Math::sqrt(Math::square(result[node_index].amplitude-expected[node_index][1]) + Math::square(result[node_index].freq-expected[node_index][0]))
+        w_err += GSL::hypot(result[node_index].amplitude-expected[node_index][1], result[node_index].freq-expected[node_index][0])
       end
       w_err = w_err / result.length
       return w_err
@@ -114,34 +131,6 @@ module OscillatorNeuralNetwork
 
   #### GA related functions ####
 
-    # This method trains the network using an instance of an OscillatorGeneticAlgorithm.
-    #   input: Network input (ordered list of data vectors)
-    #   exp_output: Expected/desired output for the given input (ordered list of data vectors)
-    #   pop_size: how many solutions to keep in the "potential solution" population
-    #   gens: how many generations to run the GA
-    #   mutation_rate: a parameter describing how frequently a given solution gets random mutations
-    # Returns a weighted error estimate of how "close" the expected and actual outputs are after
-    # training is complete.
-    def train(input, exp_output, pop_size, gens, mutation_rate=DEFAULT_MUTATION_RATE)
-      
-      change_input(input) # Set new input states
-
-      # Instantiate genetic algorithm instance
-      ga = GeneticSearch.new(self, pop_size, gens, mutation_rate)
-
-      # Run genetic algorithm, getting the best set of nodes back
-      @nodes = ga.run
-
-      # Evaluate the result with the new, GA-modified nodes in place
-      actual_output = eval
-
-      # Calculate and return a weighted error
-      err = weighted_error(actual_output, exp_output)
-
-      return err
-    end
-
- 
     # TODO fix with layers, make @curr_expected cleaner......
     # GA fitness function. Takes a nodelist and returns a weighted error calculation of the result
     # compared with the expected/desired result by evaluating the network.
@@ -171,6 +160,37 @@ module OscillatorNeuralNetwork
 
   #### Helper/convenience functions ####
 
+    # Stores connection information from the connections matrix into the nodes.
+    #
+    #   nodes:       list of OscillatorNeuron objects in order
+    #   connections: connections GSL::Matrix describing connections between nodes
+    #
+    # Returns the nodelist with connections set
+    def set_conns_from_mat(nodes, connections)
+      pointer_index = 1
+      reciever_index = 1
+      connections.each_row do |pointer|
+        pointer.each do |reciever|
+          if receiver != 0.0
+            nodes[receiver_index].in_conns = Hash.new(nodes[pointer_index], receiver)
+          end
+        reciever_index += 1
+        end
+        reciever_index = 1
+        pointer_index += 1
+      end
+      return nodes
+    end
+
+    # Retreives the current output states as a GSL::Matrix of data (ordered)
+    def get_outputs
+      start_index = @nodes.length - @num_outputs
+      @num_outputs.times do |output|
+
+      end
+      outputs = GSL::Matrix[@nodes[start_index]
+    end
+
     # Sets the input nodes to different oscillator data
     #   new_input_data: a list of vectors describing the natural frequency and amplitudes of the inputs like <freq, amp>
     def change_input(new_input_data)
@@ -187,124 +207,157 @@ module OscillatorNeuralNetwork
       # TODO run fourier analysis on vector
     end
 
-    class << Math
-      def square(num)
-        return num * num
-      end
+    def get_time_step
+      return @t_step
+    end
+
+    def get_current_time
+      return @curr_time
+    end
+
+    def increment_time
+      @curr_time += @t_step
     end
 
   end
 
   # This class describes a single OscillatorNeuron. Each neuron knows everything about
   # its own natural state, current state, next state, and inbound/outbound connections.
+  # Variable state_vector is a GSL::vector containing the following information:
+  # <a, b, x, x_prime, x_dbl_prime, layer> where the equation describing the
+  # oscillator is x_dbl_prime = -a*x - b*x_prime + input_sum
+  # so that a = spring constant, b = damping coefficient
 
   class OscillatorNeuron
-    
-    attr_accessor :x
-    attr_accessor :x_prime
-    attr_accessor :x_double_prime
 
-    attr_accessor :natural_freq
-    attr_accessor :amplitude
-
-    attr_accessor :in_conns
+    attr_accessor :state_vector
+    attr_accessor :input_sum_terms
     attr_accessor :out_conns
 
-    attr_accessor :input_sum_terms
-
     # Initialize a new OscillatorNeuron by passing a "natural state" hash.
-    #   natural_state: a list describing the "natural" frequency, amplitude, and layer as such: <layer,frequency,amplitude> 
-    def initialize(natural_state)
-      # Set states
-      @natural_freq = natural_state[1]
-      @amplitude = natural_state[2]
-      @a = 0 #TODO diff btwn a, amplitude?
-      @x, @x_prime, @x_double_prime = 0
+    #   state_vec: a GSL::Vector describing the state as given above
+    #   network_ref: a reference to the network containing this neuron
+    def initialize(state_vec, network_ref)
+      # Set state vector
+      @state_vector = state_vec
 
       # Reserve space for other instance variables
-      @in_conns = Hash.new 
       @out_conns = Hash.new
       @input_sum_terms = []
+      @network = network_ref
     end
 
-    def set_state(new_data)
-      @natural_freq = new_data[0]
-      @amplitude = new_data[1]
+    #### Accessor methods for individual elements of the state vector ####
+
+    def set_a(new_a)
+      @state_vector[0] = new_a
     end
 
-    # TODO fix, solve ODEs for new state, etc....
+    def set_b(new_b)
+      @state_vector[1] = new_b
+    end
+
+    def set_x(new_x)
+      @state_vector[2] = new_x
+    end
+
+    def set_x_prime(new_x_prime)
+      @state_vector[3] = new_x_prime
+    end
+
+    def set_x_dbl_prime(new_x_dbl_prime)
+      @state_vector[4] = new_x_dbl_prime
+    end
+
+    def get_a
+      return @state_vector[0] 
+    end
+
+    def get_b
+      return @state_vector[1]
+    end
+
+    def get_x
+      return @state_vector[2]
+    end
+
+    def get_x_prime
+      return @state_vector[3]
+    end
+
+    def get_x_dbl_prime
+      return @state_vector[4] 
+    end
+
+    def get_layer
+      return @state_vector[5]
+    end
+
+    # Updates the current state of an input node using exact solutions to the equation
+    # x_dbl_prime = -a*x (since there are no inputs to the input nodes, the added term is zero)
+    # The exact solution is x = A*sin(sqrt(a)*t + phi), where A = sqrt(x_prime^2+x^2), phi = arctan(x/x_prime)
+    def update_input_state
+      amp = GSL::hypot(get_x_prime, get_x)
+      phi = arctan(get_x/get_x_prime)
+      new_x = amp*sin(sqrt(get_a)*@network.get_current_time + phi)
+      set_x(new_x)
+    end
+
     # Updates the current state based on the current states of inputs to this node.  
     def update_state
+
+      if(get_layer == 0)
+        update_input_state
+        return
+      end
+
+      # Calculate sum of inputs
       sum = @input_sum_terms.inject(0){|sum,item| sum+item}
 
       # An oscillator
-      #   m: mass
-      #   k: spring constant
-      #   b: resist
-      #   f: external force
+      #   a: spring constant
+      #   b: damping
+      #   sum: sum of external forces, weighted by connection strength
 
       dim = 2
 
+      # Setup system of ODEs to solve
       # x[0]: displacement, x[1]: velocity
       func = Proc.new { |t, x, dxdt, params|
-        m = params[0]; b = params[1]; f = params[2]; k = params[3]
+        b = params[1] 
+        sum = params[2] 
+        a = params[3]
         dxdt[0] = x[1]
-        dxdt[1] = (f - b*x[1] - k*x[0])/m
+        dxdt[1] = (sum - b*x[1] - a*x[0])
       }
 
+      # Create solver
       gos = GSL::Odeiv::Solver.alloc(GSL::Odeiv::Step::RKF45, [1e-6, 0.0], func, dim)
 
-      m = 1.0
-      b = 1.0
-      f = 1.0
-      k = 10.0
-
-      gos.set_params(m, b, f, k)
-
-      t = 0.0; tend = 10.0
+      # Set parameters for solving
+      gos.set_params(get_b, sum, get_a)
+      t = @network.get_current_time 
+      t1 = t + @network.get_time_step
+      tend = 10.0
       h = 1e-6
-      x = GSL::Vector.alloc([0.0, 0.0])
+
+      # Initial guess vector
+      x = GSL::Vector.alloc([get_x_prime, get_x_dbl_prime])
 
       GSL::ieee_env_setup()
 
-      # Graph it!
-
-      ### 2nd version
-
-      dim = 2 #dimension of the ODE system
-
-      # Proc object to represent system
-      func = Proc.new { |t, y, dydt, a|
-        dydt[0] = y[1]
-        dydt[1] = -a*y[0] + sum 
-      }
-
-      # Create the solver
-      solver = Solver.alloc(Step::RKF45, [1e-6, 0.0], func, dim)
-      @solver.set_params(@a)
-
-      # TODO deal with time params correctly
-      t = 0.0
-      t_end = 100.0
-      h = 1e-6
-      y = GSL::Vector.aloc([1.0, 0.0]) #Initial value
-
-      while t < t_end
-        t, h, status = solver.apply(t, t1, h, y)
-
-        break if status != GSL::SUCCESS
-
-        printf("%.5e %.5e %.5e %.5e\n", t, y[0], y[1], h)
-      end
-
+      # Apply solver
+      t, h, status = gos.apply(t, t1, h, x)
+      set_x(x[0])
+      set_x_prime(x[1])
     end
 
-    # Propagates the node's curr_state to all of its out_conns
+    # Propagates the node's current x to all of its out_conns
     def propagate
       # Iterate through all outgoing connections
       @out_conns.each_key do |receiver|
         # Calculate the term of the sum corresponding to the propagating node
-        term = @out_conns[receiver] * (@x - receiver.x)
+        term = @out_conns[receiver] * (self.get_x - receiver.get_x)
         # Insert the term in the receiver's registry
         receiver.input_sum_terms << term
       end
