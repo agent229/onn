50cacf8e8dc28f09b7a18703949766263dfa8037
Natalie Klein <agent229@gmail.com>
2008-07-23 16:23:44 -0600

    new networks to test

--- test/tc_onn.rb
+++ test/tc_onn.rb
@@ -7,9 +7,27 @@ require 'gsl'
 class TestONN < Test::Unit::TestCase
 
   def setup
-    node_data = GSL::Matrix[[0.6,0,1,0.1,0,0],[0.7,0.1,0,0,0,1],[0.5,0.2,0,0,0,1],[0.3,0.1,0,0,0,2],[0.8,0.1,0,0,0,2]]
-    conns = GSL::Matrix[[0,0.4,0.4,0,0],[0,0,0.4,-0.6,0.4],[0,0.4,0,0.4,0.4],[0,0,0,0,0],[0,0,0,0,0]]
+#     node_data = GSL::Matrix[[4*GSL::M_PI*GSL::M_PI,0,1.0,0,0,0]]
+#     conns = GSL::Matrix[[0]]
+#     @net= GAONN.new(node_data,conns,1)
+
+
+#    node_data = GSL::Matrix[[4*GSL::M_PI*GSL::M_PI,0,1.0,0.2,0,0],[20.0,0,1.0,0.2,0,0],[40.0,0.05,0,0,0,1],[60.0,0.05,0,0,0,1]]
+#    conns = GSL::Matrix[[0,0,0.1,0.1],[0,0,0.1,0.1],[0,0,0,0],[0,0,0,0]]
+#    @net = GAONN.new(node_data,conns,2,0,0.01,15000)
+
+  #  node_data = GSL::Matrix[[0.9,0,1.0,0.2,0,0],[0.3,0.05,0,0,0,1],[0.7,0.05,0,0,0,1]]
+  #  conns = GSL::Matrix[[0,-0.1,-0.1],[0,0,0],[0,0,0]]
+  #  @net = GAONN.new(node_data,conns,2)
+
+  # node_data = GSL::Matrix[[0.4,0,1,0.1,0,0],[0.2,0.2,0,0,0,1],[0.3,0.2,0,0,0,1],[0.6,0.1,0,0,0,2],[1.2,0.1,0,0,0,2]]
+  # conns = GSL::Matrix[[0,0.8,0.2,0,0],[0,0,0,0.6,-0.4],[0,0,0,-0.4,0.6],[0,0,0,0,0],[0,0,0,0,0]]
+  # @net = GAONN.new(node_data,conns,2) 
+
+    node_data = GSL::Matrix[[0.7,0,1,0,0,0],[0.3,0.1,0,0,0,1],[0.8,0.1,0,0,0,1],[0.2,0.1,0,0,0,2],[0.7,0.1,0,0,0,2]]
+    conns = GSL::Matrix[[0,0.4,0.4,0,0],[0,0,0.2,0.4,-0.6],[0,0.2,0,-0.6,0.4],[0,0,0,0,0],[0,0,0,0,0]]
     @net = GAONN.new(node_data,conns,2) 
+    
   #  node_data = GSL::Matrix[[0.7,0,1,0.1,0,0],[0.7,0.1,0,0,0,1],[0.7,0.2,0,0,0,1],[0.7,0.1,0,0,0,2]]
   #  conns = GSL::Matrix[[0,0.4,0.3,0],[0,0,0,0.4],[0,0,0,0.4],[0,0,0,0]]
   #  @net = GAONN.new(node_data,conns,1)
@@ -34,16 +52,10 @@ class TestONN < Test::Unit::TestCase
 
   def test_eval
     @net.eval_over_time
-    @net.plot_x_over_time(0)
-    @net.plot_x_over_time(1)
-    @net.plot_x_over_time(2)
-    @net.plot_x_over_time(3)
-    @net.plot_x_over_time(4)
-    @net.fourier_analyze(0)
-    @net.fourier_analyze(1)
-    @net.fourier_analyze(2)
-    @net.fourier_analyze(3)
-    @net.fourier_analyze(4)
+    @net.nodes.each_index do |index|
+      @net.plot_x_over_time(index)
+      @net.fourier_analyze(index)
+    end
 #    expected = GSL::Matrix[[0,0,0,0,0,0],[0,0,0,0,0,0]] # TODO put in expected results
 #    err = @net.weighted_error(expected)
   end
