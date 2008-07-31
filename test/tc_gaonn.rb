858131ad6098225c53c4af8201cdea9806c06bf1
Natalie Klein <agent229@gmail.com>
2008-07-31 11:22:05 -0600

    new testing file

---
+++ test/tc_gaonn.rb
@@ -0,0 +1,20 @@
+# GAONN testing file
+
+require File.expand_path(File.dir(__FILE__)) + "/../ga_onn"
+require 'test/unit'
+
+class TestGAONN < Test::Unit::TestCase
+
+  def setup
+
+  end
+
+  def test_init
+
+  end
+
+  def test_run
+
+  end
+
+end
